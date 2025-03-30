3b4f1ca58e21c187d4930cbfaf6cebdb4cafc8bb
GreDDySS <misha.sudos@yandex.ru>
2025-03-30 20:44:45 +0800

    Добавлена модель Клиент для работы с данными таблицы Клиенты.

---
+++ CarWash3D/Models/Client.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarWash3D.Models
+{
+    public class Client
+    {
+        public int ID_Клиента { get; set; }
+        public string Фамилия { get; set; }
+        public string Имя { get; set; }
+        public string Отчество { get; set; }
+        public string Номер_телефона { get; set; }
+        public string Номер_машины { get; set; }
+    }
+}
