99356281e8a44853ad55d9c143a373656097cd66
GreDDySS <misha.sudos@yandex.ru>
2025-03-30 20:48:35 +0800

    Добавлена кодовая часть страницы входа с привязкой к ViewModel.

---
+++ CarWash3D/Views/LoginView.xaml.cs
@@ -0,0 +1,32 @@
+﻿using CarWash3D.ViewModels;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+
+namespace CarWash3D.Views
+{
+    public partial class LoginView : Page
+    {
+        private readonly LoginViewModel _viewModel;
+
+        public LoginView(LoginViewModel viewModel)
+        {
+            InitializeComponent();
+            _viewModel = viewModel;
+            DataContext = _viewModel;
+        }
+
+        private void PasswordBox_PasswordChanged(object sender, RoutedEventArgs e)
+        {
+            if (sender is PasswordBox passwordBox)
+            {
+                _viewModel.EmployeePassword = passwordBox.Password;
+            }
+        }
+
+        private void NumberValidationTextBox(object sender, TextCompositionEventArgs e)
+        {
+            e.Handled = !char.IsDigit(e.Text, 0);
+        }
+    }
+}
