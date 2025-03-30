a647373c7c063a5a4a2c4407e6fc850245b072a6
GreDDySS <misha.sudos@yandex.ru>
2025-03-30 20:48:02 +0800

    Добавлен ViewModel для главного окна с поддержкой навигации.

---
+++ CarWash3D/ViewModels/MainViewModel.cs
@@ -0,0 +1,14 @@
+﻿using CarWash3D.Helpers;
+
+namespace CarWash3D.ViewModels
+{
+    class MainViewModel
+    {
+        private readonly NavigationService _navigationService;
+
+        public MainViewModel(NavigationService navigationService)
+        {
+            _navigationService = navigationService;
+        }
+    }
+}
