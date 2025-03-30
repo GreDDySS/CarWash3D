f4a2cc6501ee12097ae436d6a2c8cb73040e13ac
GreDDySS <misha.sudos@yandex.ru>
2025-03-30 20:44:34 +0800

    Добавлена логика инициализации главного окна с настройкой сервисов и навигации.

--- CarWash3D/MainWindow.xaml.cs
+++ CarWash3D/MainWindow.xaml.cs
@@ -1,24 +1,33 @@
-﻿using System.Text;
+﻿using CarWash3D.Data;
+using CarWash3D.Data.Repositories;
+using CarWash3D.Helpers;
+using CarWash3D.Services;
+using CarWash3D.ViewModels;
+using CarWash3D.Views;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace CarWash3D
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
+        private readonly NavigationService _navigationService;
+
         public MainWindow()
         {
             InitializeComponent();
+
+            // Инициализация базы данных и репозиториев
+            var dbContext = new CarWashDbContext();
+            var clientRepository = new ClientRepository(dbContext);
+            var employeeRepository = new EmployeeRepository(dbContext);
+
+            // Инициализация сервисов
+            var clientService = new ClientService(clientRepository);
+            var employeeService = new EmployeeService(employeeRepository);
+            _navigationService = new NavigationService(MainFrame, clientService, employeeService);
+
+            DataContext = new MainViewModel(_navigationService);
+            _navigationService.NavigateTo("LoginView");
         }
     }
 }
