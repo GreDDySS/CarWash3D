c8e0a9f7ca1aeb1e246741cde6f9348aa99fed24
GreDDySS <misha.sudos@yandex.ru>
2025-03-30 20:46:35 +0800

    Добавлен сервис для авторизации и регистрации клиентов.

---
+++ CarWash3D/Services/ClientService.cs
@@ -0,0 +1,32 @@
+﻿using CarWash3D.Data.Repositories;
+using CarWash3D.Models;
+
+namespace CarWash3D.Services
+{
+    public class ClientService
+    {
+        private readonly ClientRepository _clientRepository;
+
+        public ClientService(ClientRepository clientRepository)
+        {
+            _clientRepository = clientRepository;
+        }
+
+        public bool AuthenticateClient(string phone, string carNumber)
+        {
+            var client = _clientRepository.GetClientByPhoneAndCarNumber(phone, carNumber);
+            return client != null;
+        }
+
+        public bool RegisterClient(Client client)
+        {
+            return _clientRepository.AddClient(client);
+        }
+
+        public bool VerifyCode(string code)
+        {
+            // Заглушка: статичный код "123456"
+            return code == "123456";
+        }
+    }
+}
