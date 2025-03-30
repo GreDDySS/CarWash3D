366f6e7791bf44b912ba313e8a97366260bb845e
GreDDySS <misha.sudos@yandex.ru>
2025-03-30 20:48:57 +0800

    Добавлена кодовая часть страницы регистрации с привязкой к ViewModel.

---
+++ CarWash3D/Views/RegistrationView.xaml.cs
@@ -0,0 +1,14 @@
+﻿using CarWash3D.ViewModels;
+using System.Windows.Controls;
+
+namespace CarWash3D.Views
+{
+    public partial class RegistrationView : Page
+    {
+        public RegistrationView(RegistrationViewModel viewModel)
+        {
+            InitializeComponent();
+            DataContext = viewModel;
+        }
+    }
+}
