023de77ec5205bb4b73c4cd1bbd7cc3213657886
GreDDySS <misha.sudos@yandex.ru>
2025-03-30 20:45:53 +0800

    Добавлен репозиторий для работы с данными сотрудников в базе данных.

---
+++ CarWash3D/Data/Repositories/EmployeeRepository.cs
@@ -0,0 +1,20 @@
+﻿using CarWash3D.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace CarWash3D.Data.Repositories
+{
+    public class EmployeeRepository
+    {
+        private readonly CarWashDbContext _context;
+
+        public EmployeeRepository(CarWashDbContext context)
+        {
+            _context = context;
+        }
+
+        public Employee GetEmployeeByUsernameAndPassword(string username, string password)
+        {
+            return _context.Сотрудники.FirstOrDefault(e => e.Логин == username && e.Пароль == password);
+        }
+    }
+}
