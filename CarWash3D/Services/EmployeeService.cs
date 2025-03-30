ed070725a9dc234bfc55c63ba50aebbf7c19d678
GreDDySS <misha.sudos@yandex.ru>
2025-03-30 20:46:59 +0800

    Добавлен сервис для авторизации сотрудников.

---
+++ CarWash3D/Services/EmployeeService.cs
@@ -0,0 +1,20 @@
+﻿using CarWash3D.Data.Repositories;
+
+namespace CarWash3D.Services
+{
+    public class EmployeeService
+    {
+        private readonly EmployeeRepository _employeeRepository;
+
+        public EmployeeService(EmployeeRepository employeeRepository)
+        {
+            _employeeRepository = employeeRepository;
+        }
+
+        public bool AuthenticateEmployee(string username, string password)
+        {
+            var employee = _employeeRepository.GetEmployeeByUsernameAndPassword(username, password);
+            return employee != null;
+        }
+    }
+}
