f3236c23ef60c0204d40f605e198cdaaf401ad56
GreDDySS <misha.sudos@yandex.ru>
2025-03-30 20:44:55 +0800

    Добавлена модель Сотрудник для работы с данными таблицы Сотрудники.

---
+++ CarWash3D/Models/Employee.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarWash3D.Models
+{
+    public class Employee
+    {
+        public int ID_Сотрудника { get; set; }
+        public string Фамилия { get; set; }
+        public string Имя { get; set; }
+        public string Отчество { get; set; }
+        public string Номер_телефона { get; set; }
+        public int ID_Должность { get; set; }
+        public string Пароль { get; set; }
+        public string Логин { get; set; }
+    }
+}
