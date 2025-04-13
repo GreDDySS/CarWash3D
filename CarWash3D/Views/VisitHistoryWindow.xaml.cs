ee880ae3377448dadb6434dc6e8e904d7a6091d0
GreDDySS <misha.sudos@yandex.ru>
2025-04-14 02:02:37 +0800

    Добавлено окно для просмотра истории посещений

---
+++ CarWash3D/Views/VisitHistoryWindow.xaml.cs
@@ -0,0 +1,15 @@
+﻿using CarWash3D.ViewModels;
+using System.Windows;
+
+namespace CarWash3D.Views
+{
+    public partial class VisitHistoryWindow : Window
+    {
+        public VisitHistoryWindow(VisitHistoryViewModel viewModel)
+        {
+            InitializeComponent();
+            DataContext = viewModel;
+
+        }
+    }
+}
