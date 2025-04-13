fe3246345f464a769e1bd56c2f149c53b71f49c5
GreDDySS <misha.sudos@yandex.ru>
2025-04-14 01:56:02 +0800

    Добавлена модель Status для статусов заказов

---
+++ CarWash3D/Models/Status.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarWash3D.Models
+{
+    public class Status
+    {
+        public int ID_Статуса { get; set; }
+        public string Название { get; set; }
+    }
+}
