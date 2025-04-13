cf46b70d9acf2836c1b8a21c94fe8cb198de3e5b
GreDDySS <misha.sudos@yandex.ru>
2025-04-14 02:02:17 +0800

    Создано окно для редактирования профиля клиента

---
+++ CarWash3D/Views/ProfileWindow.xaml.cs
@@ -0,0 +1,14 @@
+﻿using CarWash3D.ViewModels;
+using System.Windows;
+
+namespace CarWash3D.Views
+{
+    public partial class ProfileWindow : Window
+    {
+        public ProfileWindow(ProfileViewModel viewModel)
+        {
+            InitializeComponent();
+            DataContext = viewModel;
+        }
+    }
+}
