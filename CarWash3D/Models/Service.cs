0594ca13bd97710c5ce69638014acd2c340f3449
GreDDySS <misha.sudos@yandex.ru>
2025-04-14 01:55:40 +0800

    Добавлена модель Service для услуг автомойки

---
+++ CarWash3D/Models/Service.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarWash3D.Models
+{
+    public class Service
+    {
+        public int ID_Услуги { get; set; }
+        public string Наименование { get; set; }
+        public int Стоимость { get; set; }
+        public int Длительность { get; set; }
+        public string Описание { get; set; }
+        public int? ID_Акции { get; set; }
+        public Promotion Promotion { get; set; }
+    }
+}
