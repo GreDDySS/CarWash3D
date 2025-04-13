de67c946eab97aa23437d9b6352fbce9b760c4f4
GreDDySS <misha.sudos@yandex.ru>
2025-04-14 02:03:23 +0800

    Реализована инициализация приложения  с DI

--- CarWash3D/App.xaml.cs
+++ CarWash3D/App.xaml.cs
@@ -1,14 +1,58 @@
-﻿using System.Configuration;
-using System.Data;
+﻿using CarWash3D.Data;
+using CarWash3D.Data.Repositories;
+using CarWash3D.Helpers;
+using CarWash3D.Models;
+using CarWash3D.Services;
+using CarWash3D.ViewModels;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
 using System.Windows;
+using System.Windows.Controls;
 
 namespace CarWash3D
 {
-    /// <summary>
-    /// Interaction logic for App.xaml
-    /// </summary>
     public partial class App : Application
     {
+        public IServiceProvider ServiceProvider { get; private set; }
+
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+
+            var services = new ServiceCollection();
+
+            // Регистрируем DbContextFactory
+            services.AddDbContextFactory<CarWashDbContext>(options =>
+                options.UseSqlite("Data Source=Data/CarWash.db"));
+
+            // Регистрируем репозитории
+            services.AddScoped<ClientRepository>();
+            services.AddScoped<EmployeeRepository>();
+
+            // Регистрируем сервисы
+            services.AddScoped<ClientService>();
+            services.AddScoped<EmployeeService>();
+            services.AddSingleton<NavigationService>();
+
+            // Регистрируем ViewModel
+            services.AddScoped<LoginViewModel>();
+            services.AddScoped<RegistrationViewModel>();
+            // Регистрируем фабрику для CustomerCabinetViewModel
+            services.AddTransient<Func<int, CustomerCabinetViewModel>>(provider =>
+            {
+                var dbContextFactory = provider.GetRequiredService<IDbContextFactory<CarWashDbContext>>();
+                var clientService = provider.GetRequiredService<ClientService>();
+                var navigationService = provider.GetRequiredService<NavigationService>();
+                return clientId => new CustomerCabinetViewModel(dbContextFactory, clientService, navigationService, clientId);
+            });
+
+            // Регистрируем фабрику для NavigationService
+
+            ServiceProvider = services.BuildServiceProvider();
+
+            var mainWindow = new MainWindow(ServiceProvider);
+            mainWindow.Show();
+        }
     }
 
 }
