f9388e30c339f9e5ed7eeed031f0763c2fb8d3a7
GreDDySS <misha.sudos@yandex.ru>
2025-04-14 02:01:30 +0800

    Создано представление для личного кабинета клиента

---
+++ CarWash3D/Views/CustomerCabinetView.xaml.cs
@@ -0,0 +1,15 @@
+﻿using CarWash3D.ViewModels;
+using System.Windows.Controls;
+
+namespace CarWash3D.Views
+{
+    public partial class CustomerCabinetView : Page
+    {
+        public CustomerCabinetView(CustomerCabinetViewModel viewModel)
+        {
+            InitializeComponent();
+            DataContext = viewModel;
+
+        }
+    }
+}
