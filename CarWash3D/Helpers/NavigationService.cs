ef207b0379936c0db4e640b93897d15934bfc7ba
GreDDySS <misha.sudos@yandex.ru>
2025-04-14 01:51:49 +0800

    Реализован NavigationService для навигации между представлениями приложения

--- CarWash3D/Helpers/NavigationService.cs
+++ CarWash3D/Helpers/NavigationService.cs
@@ -1,43 +1,115 @@
-﻿using CarWash3D.Services;
-using CarWash3D.ViewModels;
-using CarWash3D.Views;
+﻿using System;
+using System.Collections.Generic;
 using System.Windows;
 using System.Windows.Controls;
+using CarWash3D;
+using CarWash3D.ViewModels;
+using CarWash3D.Views;
+using Microsoft.Extensions.DependencyInjection;
 
-namespace CarWash3D.Helpers
+public class NavigationService
 {
-    public class NavigationService
-    {
-        private readonly Frame _frame;
-        private readonly ClientService _clientService;
-        private readonly EmployeeService _employeeService;
+    private readonly IServiceProvider _serviceProvider;
+    private Frame _frame; // Frame теперь устанавливается позже
+    private readonly Dictionary<string, Type> _views;
+    private Func<int, CustomerCabinetViewModel> _customerCabinetViewModelFactory;
 
-        public NavigationService(Frame frame, ClientService clientService, EmployeeService employeeService)
+    public NavigationService(IServiceProvider serviceProvider)
+    {
+        _serviceProvider = serviceProvider;
+        _views = new Dictionary<string, Type>
         {
-            _frame = frame;
-            _clientService = clientService;
-            _employeeService = employeeService;
+            { "LoginView", typeof(LoginView) },
+            { "RegistrationView", typeof(RegistrationView) },
+            { "CustomerCabinetView", typeof(CustomerCabinetView) }
+        };
+    }
+
+    public void SetFrame(Frame frame)
+    {
+        _frame = frame ?? throw new ArgumentNullException(nameof(frame));
+    }
+
+    public void UpdateCustomerCabinetViewModelFactory(Func<int, CustomerCabinetViewModel> factory)
+    {
+        _customerCabinetViewModelFactory = factory;
+    }
+
+    public void NavigateTo(string viewName, object parameter = null)
+    {
+        if (_frame == null)
+        {
+            throw new InvalidOperationException("Frame is not set. Call SetFrame before navigating.");
         }
 
-        public void NavigateTo(string pageName)
+        if (_views.TryGetValue(viewName, out var viewType))
         {
-            switch (pageName)
+            // Определяем размеры окна в зависимости от представления
+            double height = 600; // Значение по умолчанию
+            double width = 450;  // Значение по умолчанию
+
+            switch (viewName)
             {
                 case "LoginView":
-                    _frame.Navigate(new LoginView(new LoginViewModel(_clientService, _employeeService, this)));
+                    height = 600;
+                    width = 450;
                     break;
                 case "RegistrationView":
-                    _frame.Navigate(new RegistrationView(new RegistrationViewModel(_clientService, this)));
+                    height = 600;
+                    width = 450;
                     break;
                 case "CustomerCabinetView":
-                    // Заглушка для личного кабинета клиента
-                    MessageBox.Show("Переход в личный кабинет клиента (пока не реализовано)");
-                    break;
-                case "DashboardView":
-                    // Заглушка для панели сотрудника
-                    MessageBox.Show("Переход в панель сотрудника (пока не реализовано)");
+                    height = 600;
+                    width = 1200;
                     break;
             }
+
+            // Получаем MainWindow через Frame
+            if (_frame.Parent is FrameworkElement element && Window.GetWindow(element) is Window window)
+            {
+                window.Height = height;
+                window.Width = width;
+            }
+
+            object view;
+            switch (viewName)
+            {
+                case "LoginView":
+                    var loginViewModel = _serviceProvider.GetRequiredService<LoginViewModel>();
+                    view = Activator.CreateInstance(viewType, loginViewModel);
+                    break;
+
+                case "RegistrationView":
+                    var registrationViewModel = _serviceProvider.GetRequiredService<RegistrationViewModel>();
+                    view = Activator.CreateInstance(viewType, registrationViewModel);
+                    break;
+
+                case "CustomerCabinetView":
+                    if (parameter is int clientId)
+                    {
+                        if (_customerCabinetViewModelFactory == null)
+                        {
+                            throw new InvalidOperationException("CustomerCabinetViewModel factory not set.");
+                        }
+                        var customerCabinetViewModel = _customerCabinetViewModelFactory(clientId);
+                        view = Activator.CreateInstance(viewType, customerCabinetViewModel);
+                    }
+                    else
+                    {
+                        throw new ArgumentException("CustomerCabinetView requires a clientId parameter of type int.");
+                    }
+                    break;
+
+                default:
+                    view = Activator.CreateInstance(viewType);
+                    break;
+            }
+
+            _frame.Content = view;
+        }
+        else
+        {
+            throw new ArgumentException($"View {viewName} not found.");
         }
     }
 }
