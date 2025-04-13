ef8813bef2f0327ac85a030fea55469dc1e0aa45
GreDDySS <misha.sudos@yandex.ru>
2025-04-14 01:55:51 +0800

    Добавлена модель ServiceOrder для заказов услуг

---
+++ CarWash3D/Models/ServiceOrder.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarWash3D.Models
+{
+    public class ServiceOrder
+    {
+        public int ID_Заказ_Услуг { get; set; }
+        public int ID_Услуги { get; set; }
+        public Service Service { get; set; }
+        public int ID_Клиента { get; set; }
+        public Client Client { get; set; }
+        public int ID_Сотрудника { get; set; }
+        public Employee Employee { get; set; }
+        public int ID_Статуса { get; set; }
+        public Status Status { get; set; }
+        public DateTime Дата_и_время { get; set; }
+    }
+}
