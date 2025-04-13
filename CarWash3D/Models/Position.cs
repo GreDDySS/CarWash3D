988f72f2e5f16c1ab21ce1c9525a0605d0480fd9
GreDDySS <misha.sudos@yandex.ru>
2025-04-14 01:54:39 +0800

    Добавлена модель Position для должностей сотрудников

---
+++ CarWash3D/Models/Position.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarWash3D.Models
+{
+    public class Position
+    {
+        public int ID_Должность { get; set; }
+        public string Наименование { get; set; }
+        public int Зарплата { get; set; }
+    }
+}
