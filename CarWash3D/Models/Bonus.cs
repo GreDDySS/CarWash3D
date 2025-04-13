1307d1d2d161d5809f611db737ac5f5337171b7e
GreDDySS <misha.sudos@yandex.ru>
2025-04-14 01:52:17 +0800

    Добавлена модель Bonus для работы с бонусами клиентов

---
+++ CarWash3D/Models/Bonus.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarWash3D.Models
+{
+    public class Bonus
+    {
+        public int ID_Бонуса { get; set; }
+        public int ID_Клиента { get; set; }
+        public Client Client { get; set; }
+        public int Кол_во { get; set; }
+        public List<BonusHistory> BonusHistories { get; set; }
+    }
+}
