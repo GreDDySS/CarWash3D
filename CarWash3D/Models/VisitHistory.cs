13340b152ccc56ad63c33b42fcff03ef93bc91b2
GreDDySS <misha.sudos@yandex.ru>
2025-04-14 01:56:12 +0800

    Добавлена модель VisitHistory для истории посещений клиентов

---
+++ CarWash3D/Models/VisitHistory.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarWash3D.Models
+{
+    public class VisitHistory
+    {
+        public int ID_Ист_Посещений { get; set; }
+        public int ID_Клиента { get; set; }
+        public Client Client { get; set; }
+        public DateTime Дата_и_время { get; set; }
+    }
+}
