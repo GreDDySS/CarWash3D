d1e355c4f43f392270fc14ec485a2829d4e5a88f
GreDDySS <misha.sudos@yandex.ru>
2025-04-14 01:55:01 +0800

    Добавлена модель ProductOrder для заказов товаров

---
+++ CarWash3D/Models/ProductOrder.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarWash3D.Models
+{
+    public class ProductOrder
+    {
+        public int ID_Заказ_Товаров { get; set; }
+        public int ID_Товара { get; set; }
+        public Product Product { get; set; }
+        public int Кол_во { get; set; }
+        public int ID_Клиента { get; set; }
+        public Client Client { get; set; }
+        public int ID_Сотрудника { get; set; }
+        public Employee Employee { get; set; }
+        public int ID_Статуса { get; set; }
+        public Status Status { get; set; }
+        public DateTime Дата_и_время { get; set; }
+    }
+}
