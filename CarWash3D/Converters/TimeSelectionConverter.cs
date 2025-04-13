6e7726dd8edecba3b351fd16639cb3806735fc60
GreDDySS <misha.sudos@yandex.ru>
2025-04-14 01:50:16 +0800

    Добавлен конвертер TimeSelectionConverter для выбора времени в интерфейсе

---
+++ CarWash3D/Converters/TimeSelectionConverter.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace CarWash3D.Converters
+{
+    public class TimeSelectionConverter : IMultiValueConverter
+    {
+        public object Convert(object[] values, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (values.Length != 2 || values[0] == null || values[1] == null)
+                return false;
+
+            string buttonContent = values[0].ToString();
+            string selectedTime = values[1].ToString();
+
+            return buttonContent == selectedTime;
+        }
+
+        public object[] ConvertBack(object value, Type[] targetTypes, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
