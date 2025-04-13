9008622a7e7f988d6230c915c2bd0be9af9794df
GreDDySS <misha.sudos@yandex.ru>
2025-04-14 02:02:01 +0800

    Добавлено окно для отображения деталей заказа

---
+++ CarWash3D/Views/OrderDetailsWindow.xaml.cs
@@ -0,0 +1,14 @@
+﻿using CarWash3D.ViewModels;
+using System.Windows;
+
+namespace CarWash3D.Views
+{
+    public partial class OrderDetailsWindow : Window
+    {
+        public OrderDetailsWindow(OrderDetailsViewModel viewModel)
+        {
+            InitializeComponent();
+            DataContext = viewModel;
+        }
+    }
+}
