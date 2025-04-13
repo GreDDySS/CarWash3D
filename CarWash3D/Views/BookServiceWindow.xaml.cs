4a91778c7749d582da060c3907fa56331464c234
GreDDySS <misha.sudos@yandex.ru>
2025-04-14 02:01:10 +0800

    Добавлено окно для записи на услуги

---
+++ CarWash3D/Views/BookServiceWindow.xaml.cs
@@ -0,0 +1,15 @@
+﻿using CarWash3D.ViewModels;
+using System.Windows;
+
+namespace CarWash3D.Views
+{
+    
+    public partial class BookServiceWindow : Window
+    {
+        public BookServiceWindow(BookServiceViewModel viewModel)
+        {
+            InitializeComponent();
+            DataContext = viewModel;
+        }
+    }
+}
