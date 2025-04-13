56cb4d119e34e8cd3b852b9f5ad74623b423dbdb
GreDDySS <misha.sudos@yandex.ru>
2025-04-14 01:55:19 +0800

    Добавлена модель Promotion для акций

---
+++ CarWash3D/Models/Promotion.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarWash3D.Models
+{
+    public class Promotion
+    {
+        public int ID_Акции { get; set; }
+        public string Наименование { get; set; }
+        public int Скидка { get; set; }
+        public DateTime Дата_и_время { get; set; }
+    }
+}
