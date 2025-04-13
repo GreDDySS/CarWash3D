e25eb0ef9d26881728773bd8b98b087cafd2de37
GreDDySS <misha.sudos@yandex.ru>
2025-04-14 01:55:29 +0800

    Добавлена модель Review для отзывов клиентов

---
+++ CarWash3D/Models/Review.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarWash3D.Models
+{
+    public class Review
+    {
+        public int ID_Отзыва { get; set; }
+        public int ID_Клиента { get; set; }
+        public Client Client { get; set; }
+        public string Отзыв { get; set; }
+        public int Оценка { get; set; }
+        public DateTime Дата_и_время { get; set; }
+    }
+}
