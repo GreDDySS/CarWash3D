b51929195334eb05b45c9f1dea051b4f436baddc
GreDDySS <misha.sudos@yandex.ru>
2025-04-14 01:52:26 +0800

    Добавлена модель BonusHistory для хранения истории операций с бонусами

---
+++ CarWash3D/Models/BonusHistory.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarWash3D.Models
+{
+    public class BonusHistory
+    {
+        public int ID_Ист_Бонусов { get; set; }
+        public int ID_Бонус { get; set; }
+        public Bonus Bonus { get; set; }
+        public string Наименование { get; set; }
+        public int Кол_во { get; set; }
+        public string Тип { get; set; }
+        public DateTime Дата_и_время { get; set; }
+    }
+}
